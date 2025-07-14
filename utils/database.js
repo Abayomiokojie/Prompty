348798210ca64cabdf5a7dbdbf41bd2d8186d94a
Abayomiokojie <Abayomiokojie@gmail.com>
2025-07-14 12:53:53 -0400

    database connection serverless update

--- utils/database.js
+++ utils/database.js
@@ -1,60 +1,23 @@
-// import mongoose from "mongoose";
+import mongoose from "mongoose";
 
-// let isConnected = false;
-// export const connectToDB = async () => {
-//     mongoose.set('strictQuery', true);
-//     if (isConnected) {
-//         console.log("MongoDB is already connected");
-//         return;
-//     }
-
-//     try {
-//         await mongoose.connect(process.env.MONGODB_URI, {
-//             dbName: "dabkinger",
-//             // useNewUrlParser: true,
-//             // useUnifiedTopology: true,
-//         });
-//         isConnected = true; // Set the connection flag - This flag helps prevent unnecessary reconnection attempts if the function is called multiple times during the application’s lifecycle.
-//         console.log("Connected to MongoDB");
-//     } catch (error) {
-//         console.log('MongoDB connection error:', error);
-//         throw error;
-//     }
-// };
-
-// utils/database.js
-import mongoose from 'mongoose';
-
-let cached = global.mongoose;
-
-if (!cached) {
-    cached = global.mongoose = { conn: null, promise: null };
-}
-
+let isConnected = false;
 export const connectToDB = async () => {
-    if (cached.conn) {
-        return cached.conn;
-    }
-
-    if (!cached.promise) {
-        const opts = {
-            bufferCommands: false,
-            maxPoolSize: 10,
-            serverSelectionTimeoutMS: 10000,
-            socketTimeoutMS: 45000,
-        };
-
-        cached.promise = mongoose.connect(process.env.MONGODB_URI, opts).then((mongoose) => {
-            return mongoose;
-        });
+    mongoose.set('strictQuery', true);
+    if (isConnected) {
+        console.log("MongoDB is already connected");
+        return;
     }
 
     try {
-        cached.conn = await cached.promise;
-    } catch (e) {
-        cached.promise = null;
-        throw e;
-    }
+        await mongoose.connect(process.env.MONGODB_URI, {
+            dbName: "dabkinger",
 
-    return cached.conn;
-}
+        });
+        isConnected = true; // Set the connection flag - This flag helps prevent unnecessary reconnection attempts if the function is called multiple times during the application’s lifecycle.
+        console.log("Connected to MongoDB");
+    } catch (error) {
+        console.log('MongoDB connection error:', error);
+        throw error;
+    }
+};
+
